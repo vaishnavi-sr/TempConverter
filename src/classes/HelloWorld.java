1fdbac0630581f536f8aa4e04d41b1b57e1f56e1
vaishnavi <vaishu.srini15@gmail.com>
2023-03-01 14:35:49 -0600

    discussed Classes and methods

---
+++ src/classes/HelloWorld.java
@@ -0,0 +1,16 @@
+package classes;
+
+public class HelloWorld {
+        public String message = "Hello World";
+    public String message1 = "Goodbye World";
+
+        public void sayHello() {
+            System.out.println(message);
+        }
+
+    public void sayGoodbye() {
+        System.out.println(message1);
+    }
+
+    }
+
---
+++ src/classes/HelloWorldRunner.java
@@ -0,0 +1,9 @@
+package classes;
+
+public class HelloWorldRunner {
+    public static void main(String[] args) {
+        HelloWorld hello = new HelloWorld();
+        hello.sayHello();
+        hello.sayGoodbye();
+    }
+}
